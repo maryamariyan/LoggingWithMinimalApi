e41a1c7b3f03fb2704081aecfd5be24082759cb8
Maryam Ariyan <maryam.ariyan@microsoft.com>
2021-10-07 15:18:59 -0700

    More samples

--- Log.cs
+++ Log.cs
@@ -1,14 +1,66 @@
 internal static partial class Log
 {
-    [LoggerMessage(EventId = 1023, Level = LogLevel.Critical, Message = "log #{LogNumber}")]
-    public static partial void LogCritical_1_Arg_Generated(ILogger logger, long logNumber);
-
     [LoggerMessage(EventId = 22, Level = LogLevel.Information, Message = "Hello World!")]
     public static partial void HelloWorld(ILogger logger);
+
+    #region Sample2
+    [LoggerMessage(EventId = 1010, Level = LogLevel.Information, Message = MessageTemplate_5_Args)]
+    public static partial void NewResidentInformationGenerated(ILogger logger,
+        string name, int age, string hometown, int yearsSince, long numSuitcases);
+
+    internal const string MessageTemplate_5_Args = "Resident {Name} aged {Age} years old" +
+        " from {Hometown} moved here {YearsSince} years with {NumSuitcases} suitcases!";
+    #endregion
+
+    #region SkippedBoilerplateCode
+    // skip all boilerplate code below by using source generated logs via LoggerMessage attribute
+    public static void NewResidentInformationUserDefined(ILogger logger, string name, int age, string hometown, int yearsSince, long numSuitcases) =>
+        s_userDefinedPerformantLog(logger, name, age, hometown, yearsSince, numSuitcases, null);
+
+    private static readonly Action<ILogger, string, int, string, int, long, Exception?> s_userDefinedPerformantLog =
+        LoggerMessage.Define<string, int, string, int, long>(
+            LogLevel.Information,
+            new EventId(1012),
+            MessageTemplate_5_Args,
+            s_LogDefineOptions);
+#endregion
+
+    #region MoreLoggerMessageSamples
+    [LoggerMessage(EventId = 111, Level = LogLevel.Information, Message = "Logged in #{ElapsedTime}")]
+    public static partial void LoggedInElapsedTime(ILogger logger, string elapsedTime);
+
+    [LoggerMessage(EventId = 1023, Level = LogLevel.Critical, Message = "log #{LogNumber}")]
+    public static partial void LogCritical_1_Arg_Generated(ILogger logger, long logNumber);
 
     [LoggerMessage(EventId = 10, Level = LogLevel.Information, Message = "Got weather forecast!")]
     public static partial void GotWeatherForecast(ILogger logger);
 
     [LoggerMessage(EventId = 0, EventName = "GotWeatherForecastWithSummary", Level = LogLevel.Information, Message = "Got weather forecast, and it's {Summary}!")]
     public static partial void GotWeatherForecastWithSummary(ILogger logger, string summary);
+    #endregion
+
+    #region SkipEnabledCheckFeature
+    public static void SkipEnabledCheckFalse(ILogger logger) =>
+        s_SkipEnabledCheckFalse(logger, null);
+
+    private static readonly Action<ILogger, Exception?> s_SkipEnabledCheckFalse =
+        LoggerMessage.Define(
+            LogLevel.Information,
+            new EventId(1011),
+            "Hello World",
+            s_LogDefineOptionsSkipFalse);
+
+    public static void SkipEnabledCheckTrue(ILogger logger) =>
+        s_SkipEnabledCheckTrue(logger, null);
+
+    private static readonly Action<ILogger, Exception?> s_SkipEnabledCheckTrue =
+        LoggerMessage.Define(
+            LogLevel.Information,
+            new EventId(1010),
+            "Hello World",
+            s_LogDefineOptions);
+
+    private static LogDefineOptions s_LogDefineOptions = new LogDefineOptions() {  SkipEnabledCheck = true };
+    private static LogDefineOptions s_LogDefineOptionsSkipFalse = new LogDefineOptions() {  SkipEnabledCheck = false };
+    #endregion
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 using Demo;
 using Microsoft.Extensions.Logging.Console;
-using System.Text.Json;
+using System.Diagnostics;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -28,6 +28,10 @@ var summaries = new[]
 {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
+var residents = new[]
+{
+    "Sam", "Tina", "Chester", "Brian"
+};
 
 app.MapGet("/", () =>
 {
@@ -49,14 +53,77 @@ app.MapGet("/weatherforecast", () =>
     {
         Log.GotWeatherForecastWithSummary(app.Logger, forecastOnDay.Summary!);
     }
-    
+
     return forecasts;
 })
 .WithName("GetWeatherForecast");
 
+app.MapGet("/residentinfo", () =>
+{
+    var resident = new Resident
+    (
+        residents[Random.Shared.Next(residents.Length)],
+        Random.Shared.Next(19, 55),
+        "Vancouver",
+        Random.Shared.Next(1, 11),
+        2
+    );
+
+    // fast to write, slow to execute
+    app.Logger.LogInformation(1010, Log.MessageTemplate_5_Args, resident.Name, resident.Age, resident.Hometown, resident.YearsSince, resident.NumSuitcases);
+
+    // slow to write, faster to execute
+    Log.NewResidentInformationUserDefined(app.Logger, resident.Name, resident.Age, resident.Hometown, resident.YearsSince, resident.NumSuitcases);
+
+    // fast to write and to execute
+    Log.NewResidentInformationGenerated(app.Logger, resident.Name, resident.Age, resident.Hometown, resident.YearsSince, resident.NumSuitcases);
+})
+.WithName("GetResidentInformantion");
+
+#region SkipEnabledCheckFeature
+app.MapGet("/skipenabledcheckfalse", () =>
+{
+    CheckElapsedTime(() =>
+    {
+        for (int i = 0; i < 100_000; i++)
+            Log.SkipEnabledCheckFalse(app.Logger);
+    });
+})
+.WithName("SkipEnabledCheckFalse");
+
+app.MapGet("/skipenabledchecktrue", () =>
+{
+    CheckElapsedTime(() =>
+    {
+        for (int i = 0; i < 100_000; i++)
+            Log.SkipEnabledCheckTrue(app.Logger);
+    });
+})
+.WithName("SkipEnabledCheckTrue");
+#endregion
+
 app.Run();
+
+void CheckElapsedTime(Action action)
+{
+    Stopwatch stopWatch = new Stopwatch();
+    stopWatch.Start();
+    action();
+    stopWatch.Stop();
+    // Get the elapsed time as a TimeSpan value.
+    TimeSpan ts = stopWatch.Elapsed;
+
+    // Format and display the TimeSpan value.
+    string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+        ts.Hours, ts.Minutes, ts.Seconds,
+        ts.Milliseconds / 10);
+
+    Log.LoggedInElapsedTime(app.Logger, elapsedTime);
+}
 
 record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
+
+record Resident(string Name, int Age, string Hometown, int YearsSince, long NumSuitcases) { }
